cc0df1076b4e9415bd899224e6ef7e4c439fca42
Martin Liberg <mr.liberg@gmail.com>
2014-07-06 17:06:03 +0200

    fixed sliding

--- www/script.js
+++ www/script.js
@@ -1,57 +1,31 @@
-var $app = $('.app'),
-  hApp = Hammer($app.get(0)),
-  current = {
-    position: {
-      x: 0,
-      y: 0
-    },
-    axis: null
-  },
-  xAndYRegex = /,\s([\-\d]*),\s([\-\d]*)\)$/;
+var current = {
+  x: 0,
+  y: 0
+};
+var $app = $('.app');
 
+var min = $app.find('.location').length * window.innerWidth * -1;
+var max = 0;
 
 function move(x, y) {
-  x = x || current.position.x + 'px';
-  y = y || current.position.y + 'px';
-  $app.css('-webkit-transform', 'translate3d(' + x + ', ' + y + ', 0)');
+  if (x !== undefined && (x === min || x > max)) {
+    return;
+  }
+
+  current.x = x !== undefined ? x : current.x;
+  current.y = y !== undefined ? y : current.y;
+  $app.css('-webkit-transform', 'translate3d(' + current.x + 'px, ' + current.y + 'px, 0)');
 }
 
-hApp.on('dragstart', function (event) {
-  var match = $app.css('-webkit-transform').match(xAndYRegex), // returns "matrix(1, 0, 0, 1, 0, 0)"
-    dir = event.gesture.direction;
-
-  current.position.x = match ? parseInt(match[1]) : 0
-  current.position.y = match ? parseInt(match[2]) : 0
-  current.axis = dir === 'up' || dir === 'down' ? 'vertical' : 'horizontal';
-
-  $app.removeClass('app-transition');
-});
-
-hApp.on('dragdown dragup', function (event) {
-  if (current.axis === 'vertical') {
-    var value = event.gesture.deltaY + current.position.y;
-    move(null, value + 'px');
+Hammer(document.body).on('dragend', function (event) {
+  switch (event.gesture.direction) {
+  case 'up':
+    move(undefined, window.innerHeight * -1); break;
+  case 'down':
+    move(undefined, 0); break;
+  case 'left':
+    move(current.x - window.innerWidth, undefined); break;
+  case 'right':
+    move(current.x + window.innerWidth, undefined); break;
   }
-});
-
-hApp.on('dragleft dragright', function (event) {
-  if (current.axis === 'horizontal') {
-    var value = event.gesture.deltaX + current.position.x;
-    move(value + 'px', null);
-  }
-});
-
-hApp.on('dragend', function (event) {
-    $app.addClass('app-transition');
-
-    switch (event.gesture.direction) {
-      case 'up':
-        move(null, '-100%'); break;
-      case 'down':
-        move(null, '0px'); break;
-      case 'left':
-        move('-100%', null); break;
-      case 'right':
-        move('0px', null); break;
-    }
 });
