6d5182f1e2e48b02c2d1d509d46c80e26e7c224c
Akekarat <tarakerat@gmail.com>
2014-08-26 12:04:08 +0200

    update plugin that send data to firebase

--- plugins/com.themobilelife.queue-estimote-background/src/ios/EstimoteBackground.h
+++ plugins/com.themobilelife.queue-estimote-background/src/ios/EstimoteBackground.h
@@ -21,25 +21,6 @@
 
 
 
-#pragma mark - FileWriter
-
-
-
-
-// This will return the file contents in a JSON object via the getFileContents utility method
-- (void) cordovaGetFileContents:(CDVInvokedUrlCommand *)command;
-
-// This will accept a String and call setFileContents to persist the String on to disk
-- (void) cordovaSetFileContents:(CDVInvokedUrlCommand *)command;
-
-#pragma mark - Util_Methods
-
-// Pure native code to persist data
-//- (void) setFileContents;
-
-// Native code to load data from disk and return the String.
-- (NSString *) getFileContents;
-
 
 
 @end
--- plugins/com.themobilelife.queue-estimote-background/src/ios/EstimoteBackground.m
+++ plugins/com.themobilelife.queue-estimote-background/src/ios/EstimoteBackground.m
@@ -8,6 +8,17 @@
 
 #import "EstimoteBackground.h"
 
+#import <Firebase/Firebase.h>
+
+#define RANGE_CLOSE_METER (0.3f)
+
+typedef enum : NSUInteger {
+    EBBEACON_START_STATE,
+    EBBEACON_QUEUE_STATE,
+    EBBEACON_QUEUE_WAITINGDATA_STATE,
+    EBBEACON_END_STATE
+} EBBeaconState;
+
 @interface EstimoteBackground () <ESTBeaconManagerDelegate, CLLocationManagerDelegate>
 
 
@@ -20,6 +31,15 @@
 
 @property (readwrite) UIBackgroundTaskIdentifier bgTask;
 
+@property (nonatomic, strong) Firebase* rootFirebase;
+
+@property (readwrite) EBBeaconState beaconState;
+
+@property (nonatomic, strong) NSString* queueSessionKey;
+
+@property (nonatomic, strong) NSString* currentBeaconID;
+@property (nonatomic, strong) NSNumber* startTime;
+
 @end
 
 @implementation EstimoteBackground
@@ -28,6 +48,53 @@
 - (void) cordovaInitEstimote:(CDVInvokedUrlCommand *)command {
     
     NSLog(@"cordovaInitEstimote");
+    
+    self.beaconState = EBBEACON_START_STATE;
+    
+    
+    
+    self.rootFirebase = [[Firebase alloc] initWithUrl:@"https://queue-app.firebaseio.com/"];
+    
+    Firebase* firebaseDeviceID = [self.rootFirebase childByAutoId];
+    
+    NSLog(@"firebaseDeviceID = %@", firebaseDeviceID);
+    
+    
+//    NSString* uuid1 = [self uuidString];
+    __block NSString* uuid = [self uniqueAppInstanceIdentifier];
+    
+    
+    NSLog(@"uuid = %@", uuid);
+    
+    
+    
+    
+//    [self.rootFirebase observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+////        NSLog(@"%@", snapshot.value);
+////        NSLog(@"%@", snapshot.value[@"users"]);
+////        NSLog(@"%@", snapshot.value[@"users"][uuid]);
+//        
+//        
+//        
+////        {// add gender
+////            
+////            Firebase* userFirebase = [[Firebase alloc] initWithUrl:@"https://queue-app.firebaseio.com/users"];
+////            
+////            Firebase* uuidFirebase = [userFirebase childByAppendingPath:uuid];
+////            
+////            [uuidFirebase setValue:@{@"gender":@"male"}];
+////        }
+//        
+//        
+//        
+//        
+//    } withCancelBlock:^(NSError *error) {
+//        NSLog(@"error %@", error.description);
+//    }];
+    
+    
+    
+    
     
     
     
@@ -38,14 +105,10 @@
     
     self.currentRegion.notifyEntryStateOnDisplay = YES;
     
-    
-    NSLog(@"self.currentRegion = %@", self.currentRegion);
-//    NSLog(@"self.beacon.proximityUUID = %@", self.beacon.proximityUUID);
-    
     // start discovery
     [self.beaconManager startEstimoteBeaconsDiscoveryForRegion:self.currentRegion];
     
-    NSLog(@"self.beacons = %@", self.beacons);
+    
     [self.beaconManager startMonitoringForRegion:self.currentRegion];
     [self.beaconManager startRangingBeaconsInRegion:self.currentRegion];
     [self.beaconManager requestStateForRegion:self.currentRegion];
@@ -54,6 +117,7 @@
     
     self.bgTask = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
         
+        [[UIApplication sharedApplication] endBackgroundTask:self.bgTask];
     }];
     
     [self performSelector:@selector(endBGTask) withObject:nil afterDelay:3600];
@@ -74,25 +138,39 @@
 }
 
 
+/// copy from phonegap code
+- (NSString*)uniqueAppInstanceIdentifier
+{
+    NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+    static NSString* UUID_KEY = @"CDVUUID";
+    
+    NSString* app_uuid = [userDefaults stringForKey:UUID_KEY];
+    
+    if (app_uuid == nil) {
+        CFUUIDRef uuidRef = CFUUIDCreate(kCFAllocatorDefault);
+        CFStringRef uuidString = CFUUIDCreateString(kCFAllocatorDefault, uuidRef);
+        
+        app_uuid = [NSString stringWithString:(__bridge NSString*)uuidString];
+        [userDefaults setObject:app_uuid forKey:UUID_KEY];
+        [userDefaults synchronize];
+        
+        CFRelease(uuidString);
+        CFRelease(uuidRef);
+    }
+    
+    return app_uuid;
+}
+
+
+
 #pragma mark - CLLocationManager delegate
 
 - (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region
 {
-    if(state == CLRegionStateInside) {
-        NSLog(@"locationManager didDetermineState INSIDE for %@", region.identifier);
-    }
-    else if(state == CLRegionStateOutside) {
-        NSLog(@"locationManager didDetermineState OUTSIDE for %@", region.identifier);
-    }
-    else {
-        NSLog(@"locationManager didDetermineState OTHER for %@", region.identifier);
-    }
 }
 
 - (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
 {
-    // I commented out the line below because otherwise you see this every second in the logs
-    NSLog(@"locationManager didRangeBeacons");
 }
 
 
@@ -101,150 +179,175 @@
 
 - (void)beaconManager:(ESTBeaconManager *)manager didEnterRegion:(ESTBeaconRegion *)region
 {
-    NSLog(@"didEnterRegion ");
-    UILocalNotification *notification = [UILocalNotification new];
-    notification.alertBody = @"Enter region notification";
-    
-    [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
 }
 
 - (void)beaconManager:(ESTBeaconManager *)manager didExitRegion:(ESTBeaconRegion *)region
 {
-    NSLog(@"didExitRegion ");
-    UILocalNotification *notification = [UILocalNotification new];
-    notification.alertBody = @"Exit region notification";
-    
-    [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
 }
 
 - (void)beaconManager:(ESTBeaconManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(ESTBeaconRegion *)region
 {
-    ESTBeacon *firstBeacon = [beacons firstObject];
     
     
     for(ESTBeacon* beacon in beacons){
         
+        
+        if(self.beaconState == EBBEACON_START_STATE){
+            [self updateStartTime: beacon];
+            
+            self.beaconState = EBBEACON_QUEUE_STATE;
+        }
+        
         NSLog(@"major: %@, distance: %@, proximity: %d", beacon.major, beacon.distance, beacon.proximity);
         
-        if([beacon.distance floatValue] < 0.3f){
+        if(self.beaconState == EBBEACON_QUEUE_STATE && [beacon.distance floatValue] < RANGE_CLOSE_METER){
             
             UILocalNotification *notification = [UILocalNotification new];
-            notification.alertBody = [NSString stringWithFormat:@"%@: distance < 0.3", beacon.major];
+            notification.alertBody = [NSString stringWithFormat:@"%@: distance < %f", beacon.major, RANGE_CLOSE_METER];
             
             [[UIApplication sharedApplication] presentLocalNotificationNow:notification];
+            
+            
+            
+            
+            
+            [self updateEndTime: beacon withBlock:^{
+                
+                NSLog(@"self.bgTask = %d", self.bgTask);
+                
+                
+                [self performSelector:@selector(endBGTask) withObject:nil afterDelay:5];
+                
+                self.beaconState = EBBEACON_END_STATE;
+            }];
+            
+            
+            
+            
+            
+            
+            
         }
+        
+        
+        
     }
     
-//    [self updateDotPositionForDistance:[firstBeacon.distance floatValue]];
+//    double timeRemaining = [UIApplication sharedApplication].backgroundTimeRemaining;
+//    NSLog(@"timeRemaining = %f",timeRemaining);
+    
+    
 }
 
-
-
-
-
-
-
-
-#pragma mark - FileWriter
-
-
-
-- (void) cordovaGetFileContents:(CDVInvokedUrlCommand *)command {
+- (void) updateStartTime:(ESTBeacon*) beacon{
     
-    // Retrieve the date String from the file via a utility method
-    NSString *dateStr = [self getFileContents];
+    NSString* beaconID = [self getBeaconID:beacon];
     
-    // Create an object that will be serialized into a JSON object.
-    // This object contains the date String contents and a success property.
-    NSDictionary *jsonObj = [ [NSDictionary alloc]
-                             initWithObjectsAndKeys :
-                             dateStr, @"dateStr",
-                             @"true", @"success",
-                             nil
-                             ];
+    self.currentBeaconID = beaconID;
     
-    // Create an instance of CDVPluginResult, with an OK status code.
-    // Set the return message as the Dictionary object (jsonObj)...
-    // ... to be serialized as JSON in the browser
-    CDVPluginResult *pluginResult = [ CDVPluginResult
-                                     resultWithStatus    : CDVCommandStatus_OK
-                                     messageAsDictionary : jsonObj
-                                     ];
+//    NSString* queueURL = [NSString stringWithFormat:@"https://queue-app.firebaseio.com/queueSessions/%@", beaconID];
+//    
+//    NSLog(@"queueURL = %@", queueURL);
+//    //        self.queueSessionKey
+//    Firebase* queueSessionsFirebase = [[Firebase alloc] initWithUrl:queueURL];
     
-    // Execute sendPluginResult on this plugin's commandDelegate, passing in the ...
-    // ... instance of CDVPluginResult
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    
+//    NSLog(@"queueSessionsFirebase = %@", queueSessionsFirebase);
+    
+    
+    NSNumber* startTime =  @((int)[[NSDate date] timeIntervalSince1970]);
+    NSLog(@"startTime = %@", startTime);
+    self.startTime = startTime;
+    
+    
+    
+    
+//    Firebase* childAutoIDFirebase = [queueSessionsFirebase childByAutoId];
+//    
+//    NSString* autoID = childAutoIDFirebase.description;
+//    NSLog(@"autoID = %@",autoID);
+//    self.queueSessionKey = autoID;
+//    
+//    [childAutoIDFirebase setValue:@{@"start" : startTime}];
+    
+    
+    
 }
 
-- (void) cordovaSetFileContents:(CDVInvokedUrlCommand *)command {
-    // Retrieve the JavaScript-created date String from the CDVInvokedUrlCommand instance.
-    // When we implement the JavaScript caller to this function, we'll see how we'll
-    // pass an array (command.arguments), which will contain a single String.
-    NSString *dateStr = [command.arguments objectAtIndex:0];
+- (void) updateEndTime:(ESTBeacon*) beacon withBlock:(void (^)(void))successBlock{
     
-    // We call our setFileContents utility method, passing in the date String
-    // retrieved from the command.arguments array.
-    [self setFileContents: dateStr];
+    NSLog(@"updateEndTime");
     
-    // Create an object with a simple success property.
-    NSDictionary *jsonObj = [ [NSDictionary alloc]
-                             initWithObjectsAndKeys :
-                             @"true", @"success",
-                             nil
-                             ];
+    __block NSString* beaconID = [self getBeaconID:beacon];
     
-    CDVPluginResult *pluginResult = [ CDVPluginResult
-                                     resultWithStatus    : CDVCommandStatus_OK
-                                     messageAsDictionary : jsonObj
-                                     ];
+    NSLog(@"1 beaconID = %@", beaconID);
     
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    if(self.currentBeaconID && [beaconID isEqualToString:self.currentBeaconID]){
+        
+        __block NSString* uuid = [self uniqueAppInstanceIdentifier];
+        
+        self.beaconState = EBBEACON_QUEUE_WAITINGDATA_STATE;
+        
+//        NSString* queueURL = [NSString stringWithFormat:@"https://queue-app.firebaseio.com/queueSessions/%@", beaconID];
+//        Firebase* queueSessionsFirebase = [[Firebase alloc] initWithUrl:queueURL];
+        
+        __block ESTBeacon* beaconBlock = beacon;
+        
+        [self.rootFirebase observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+            
+            if(self.beaconState == EBBEACON_QUEUE_WAITINGDATA_STATE){
+                NSLog(@"observeEventType");
+                
+                id users = snapshot.value[@"users"];
+                id userUUID = users[uuid];
+                
+                //        NSLog(@"users = %@", users);
+                //        NSLog(@"userUUID = %@", userUUID);
+                
+                NSString* gender = @"";
+                if(userUUID){
+                    gender = userUUID[@"gender"];
+                }
+                
+                ///
+                NSString* beaconID = [self getBeaconID:beaconBlock];
+                
+                NSLog(@"2 beaconID = %@", beaconID);
+                
+                NSString* queueURL = [NSString stringWithFormat:@"https://queue-app.firebaseio.com/queueSessions/%@", beaconID];
+                
+                Firebase* queueSessionsFirebase = [[Firebase alloc] initWithUrl:queueURL];
+//
+                
+                Firebase* childAutoIDFirebase = [queueSessionsFirebase childByAutoId];
+                
+                NSLog(@"childAutoIDFirebase = %@", childAutoIDFirebase);
+                
+                NSNumber* endTime =  @((int)[[NSDate date] timeIntervalSince1970]);
+                
+                if(gender){
+                    [childAutoIDFirebase setValue:@{@"start":self.startTime, @"end":endTime, @"gender": gender}];
+                }else{
+                    [childAutoIDFirebase setValue:@{@"start":self.startTime, @"end":endTime}];
+                }
+                
+                successBlock();
+            }
+            
+        } withCancelBlock:^(NSError *error) {
+            NSLog(@"error %@", error.description);
+        }];
+    }
+    
+    
+    
+    
 }
 
-#pragma mark - Util_Methods
-// Dives into the file system and writes the file contents.
-// Notice fileContents as the first argument, which is of type NSString
-- (void) setFileContents:(NSString *)fileContents {
-    
-    // Create an array of directory Paths, to allow us to get the documents directory
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    
-    // The documents directory is the first item
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    
-    // Create a string that prepends the documents directory path to a text file name
-    // using NSString's stringWithFormat method.
-    NSString *fileName = [NSString stringWithFormat:@"%@/myTextFile.txt", documentsDirectory];
-    
-    // Here we save contents to disk by executing the writeToFile method of
-    // the fileContents String, which is the first argument to this function.
-    [fileContents writeToFile : fileName
-                  atomically  : NO
-                  encoding    : NSStringEncodingConversionAllowLossy
-                  error       : nil];
+- (NSString*) getBeaconID:(ESTBeacon*) beacon{
+    return [NSString stringWithFormat:@"%d-%d", [beacon.major intValue], [beacon.minor intValue]];
 }
 
-//Dives into the file system and returns contents of the file
-- (NSString *) getFileContents{
-    
-    // These next three lines should be familiar to you.
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    
-    NSString *fileName = [NSString stringWithFormat:@"%@/myTextFile.txt", documentsDirectory];
-    
-    // Allocate a string and initialize it with the contents of the file via
-    // the initWithContentsOfFile instance method.
-    NSString *fileContents = [[NSString alloc]
-                              initWithContentsOfFile : fileName
-                              usedEncoding           : nil
-                              error                  : nil
-                              ];
-    
-    // Return the file contents String to the caller (cordovaGetFileContents)
-    return fileContents;
-}
 
 
 
