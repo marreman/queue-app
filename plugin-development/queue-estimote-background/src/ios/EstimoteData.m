cc8bf5d66dde05ae7aa4baee3da2a68bba88c6bd
Akekarat <tarakerat@gmail.com>
2014-08-29 15:36:14 +0200

    keep track all estimote separately

---
+++ plugin-development/queue-estimote-background/src/ios/EstimoteData.m
@@ -0,0 +1,13 @@
+//
+//  EstimoteData.m
+//  queue-estimote-background
+//
+//  Created by Akekarat Pattrasitidecha on 8/29/14.
+//
+//
+
+#import "EstimoteData.h"
+
+@implementation EstimoteData
+
+@end
